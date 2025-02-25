60386a5095d51dcc287dd5f4cf73d5344dd6bc59
leelima mahajan <128314370+Lisaluck@users.noreply.github.com>
2025-02-25 12:35:01 +0530

    Update product.py

--- limma/models/product.py
+++ limma/models/product.py
@@ -1,20 +1,19 @@
 from django.db import models
-STATUS_CHOICE=(
-    ('Accepted','Accepted'),
-    ('Packed','Packed'),
-    ('on the way','on the way'),
-    ('Delivered','Delivered'),
-    ('cancel','cancel'),
+from .category import Category
 
-)
-class OrderDetail(models.Model):
-    user=models.IntegerField(default=True)
-    product_name=models.CharField(max_length=250)
-    image=models.ImageField(null=True,blank=True)
-    qty=models.PositiveIntegerField(default=1)
-    price=models.IntegerField()
-    ordered_date=models.DateTimeField(auto_now_add=True)
-    status= models.CharField(max_length=50,default='pending',choices=STATUS_CHOICE)
-
+class Product(models.Model):
+    name=models.CharField(max_length=600)
+    price=models.IntegerField(default=0)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE,default=1)
+    image=models.ImageField(upload_to='uploadedtoo/products/')
+    description=models.CharField(max_length=1000,blank=True)
+    @staticmethod
+    def get_all_product():
+        return Product.objects.all()
+    def get_all_product_by_category(Category_id):
+        if Category_id:
+            return Product.objects.filter(category=Category_id)
+        else:
+            return Product.get_all_product()
     def __str__(self):
-        return f"Order({self.product_name}, {self.status})"
+        return self.name
